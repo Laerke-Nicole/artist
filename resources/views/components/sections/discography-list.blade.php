2f6a7c3199a17407964ea8c9332fdc663f719fe9
Lærke Nielsen <nicolenielsen85@gmail.com>
2026-02-11 00:37:32 +0100

    styling discography page

---
+++ resources/views/components/sections/discography-list.blade.php
@@ -0,0 +1,93 @@
+<div class="section-spacing">
+    <div class="container mb-5">
+        <h2 class="mb-4">Discography</h2>
+        <div class="d-flex flex-column flex-md-row justify-content-between align-items-start align-items-md-center gap-4">
+            <div class="d-flex gap-2 flex-wrap">
+                <a href="" class="btn btn-sm btn-light">Albums</a>
+                <a href="" class="btn btn-sm btn-outline-light">Eps</a>
+                <a href="" class="btn btn-sm btn-outline-light">Singles</a>
+                <a href="" class="btn btn-sm btn-outline-light">Collabs</a>
+            </div>
+            <div>
+                <a href="" class="btn btn-outline-light">Sort by</a>
+            </div>
+        </div>
+    </div>
+
+    <div class="container-fluid">
+        <div class="row g-2">
+            <div class="col-md-6 col-lg-3">
+                <div class="position-relative img-hover-scale-above">
+                    <img src="https://picsum.photos/500/500?random=8" alt="" class="w-100">
+                    <p class="img-hover-scale-above__content px-3 px-lg-2 mb-lg-0 fs-3 position-absolute bottom-0 start-0 text-uppercase fw-bold"><span class="text-white">When we all fall asleep where do we go?</span><br>2019</p>
+                </div>
+            </div>
+            <div class="col-md-6 col-lg-3">
+                <div class="position-relative img-hover-scale-above">
+                    <img src="https://picsum.photos/500/500?random=8" alt="" class="w-100">
+                    <p class="img-hover-scale-above__content px-3 px-lg-2 mb-lg-0 fs-3 position-absolute bottom-0 start-0 text-uppercase fw-bold"><span class="text-white">When we all fall asleep where do we go?</span><br>2019</p>
+                </div>
+            </div>
+            <div class="col-md-6 col-lg-3">
+                <div class="position-relative img-hover-scale-above">
+                    <img src="https://picsum.photos/500/500?random=8" alt="" class="w-100">
+                    <p class="img-hover-scale-above__content px-3 px-lg-2 mb-lg-0 fs-3 position-absolute bottom-0 start-0 text-uppercase fw-bold"><span class="text-white">When we all fall asleep where do we go?</span><br>2019</p>
+                </div>
+            </div>
+            <div class="col-md-6 col-lg-3">
+                <div class="position-relative img-hover-scale-above">
+                    <img src="https://picsum.photos/500/500?random=8" alt="" class="w-100">
+                    <p class="img-hover-scale-above__content px-3 px-lg-2 mb-lg-0 fs-3 position-absolute bottom-0 start-0 text-uppercase fw-bold"><span class="text-white">When we all fall asleep where do we go?</span><br>2019</p>
+                </div>
+            </div>
+            <div class="col-md-6 col-lg-3">
+                <div class="position-relative img-hover-scale-above">
+                    <img src="https://picsum.photos/500/500?random=8" alt="" class="w-100">
+                    <p class="img-hover-scale-above__content px-3 px-lg-2 mb-lg-0 fs-3 position-absolute bottom-0 start-0 text-uppercase fw-bold"><span class="text-white">When we all fall asleep where do we go?</span><br>2019</p>
+                </div>
+            </div>
+            <div class="col-md-6 col-lg-3">
+                <div class="position-relative img-hover-scale-above">
+                    <img src="https://picsum.photos/500/500?random=8" alt="" class="w-100">
+                    <p class="img-hover-scale-above__content px-3 px-lg-2 mb-lg-0 fs-3 position-absolute bottom-0 start-0 text-uppercase fw-bold"><span class="text-white">When we all fall asleep where do we go?</span><br>2019</p>
+                </div>
+            </div>
+            <div class="col-md-6 col-lg-3">
+                <div class="position-relative img-hover-scale-above">
+                    <img src="https://picsum.photos/500/500?random=8" alt="" class="w-100">
+                    <p class="img-hover-scale-above__content px-3 px-lg-2 mb-lg-0 fs-3 position-absolute bottom-0 start-0 text-uppercase fw-bold"><span class="text-white">When we all fall asleep where do we go?</span><br>2019</p>
+                </div>
+            </div>
+            <div class="col-md-6 col-lg-3">
+                <div class="position-relative img-hover-scale-above">
+                    <img src="https://picsum.photos/500/500?random=8" alt="" class="w-100">
+                    <p class="img-hover-scale-above__content px-3 px-lg-2 mb-lg-0 fs-3 position-absolute bottom-0 start-0 text-uppercase fw-bold"><span class="text-white">When we all fall asleep where do we go?</span><br>2019</p>
+                </div>
+            </div>
+            <div class="col-md-6 col-lg-3">
+                <div class="position-relative img-hover-scale-above">
+                    <img src="https://picsum.photos/500/500?random=8" alt="" class="w-100">
+                    <p class="img-hover-scale-above__content px-3 px-lg-2 mb-lg-0 fs-3 position-absolute bottom-0 start-0 text-uppercase fw-bold"><span class="text-white">When we all fall asleep where do we go?</span><br>2019</p>
+                </div>
+            </div>
+            <div class="col-md-6 col-lg-3">
+                <div class="position-relative img-hover-scale-above">
+                    <img src="https://picsum.photos/500/500?random=8" alt="" class="w-100">
+                    <p class="img-hover-scale-above__content px-3 px-lg-2 mb-lg-0 fs-3 position-absolute bottom-0 start-0 text-uppercase fw-bold"><span class="text-white">When we all fall asleep where do we go?</span><br>2019</p>
+                </div>
+            </div>
+            <div class="col-md-6 col-lg-3">
+                <div class="position-relative img-hover-scale-above">
+                    <img src="https://picsum.photos/500/500?random=8" alt="" class="w-100">
+                    <p class="img-hover-scale-above__content px-3 px-lg-2 mb-lg-0 fs-3 position-absolute bottom-0 start-0 text-uppercase fw-bold"><span class="text-white">When we all fall asleep where do we go?</span><br>2019</p>
+                </div>
+            </div>
+            <div class="col-md-6 col-lg-3">
+                <div class="position-relative img-hover-scale-above">
+                    <img src="https://picsum.photos/500/500?random=8" alt="" class="w-100">
+                    <p class="img-hover-scale-above__content px-3 px-lg-2 mb-lg-0 fs-3 position-absolute bottom-0 start-0 text-uppercase fw-bold"><span class="text-white">When we all fall asleep where do we go?</span><br>2019</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
