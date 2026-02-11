e308f326abb4b7a287295e22f220e4e8c09d0440
Lærke Nielsen <nicolenielsen85@gmail.com>
2026-02-11 14:45:47 +0100

    styling shopping cards

--- resources/views/components/sections/shop-shortcuts.blade.php
+++ resources/views/components/sections/shop-shortcuts.blade.php
@@ -2,15 +2,15 @@
     <div class="row gy-4">
         <div class="col-md-6 col-lg-3">
             <a href="">
-                <div class="border border-light p-2 img-hover-scale">
-                    <img src="https://picsum.photos/300/250?random=1" alt="" class="img-hover-scale mb-3 w-100">
+                <div class="border border-light p-2 hover-rotate">
+                    <img src="https://picsum.photos/300/250?random=1" alt="" class="mb-3 w-100">
 
                     <div class="d-flex justify-content-between align-items-center mx-3">
                         <div>
                             <p class="fs-4 text-uppercase fw-bold text-white">All merch</p>
                         </div>
                         <div>
-                            <p class="text-uppercase">View</p>
+                            <p class="text-uppercase link-light">View</p>
                         </div>
                     </div>
                 </div>
@@ -18,15 +18,15 @@
         </div>
         <div class="col-md-6 col-lg-3">
             <a href="">
-                <div class="border border-light p-2 img-hover-scale">
-                    <img src="https://picsum.photos/300/250?random=1" alt="" class="img-hover-scale mb-3 w-100">
+                <div class="border border-light p-2 hover-rotate">
+                    <img src="https://picsum.photos/300/250?random=1" alt="" class="mb-3 w-100">
 
                     <div class="d-flex justify-content-between align-items-center mx-3">
                         <div>
                             <p class="fs-4 text-uppercase fw-bold text-white">Outerwear</p>
                         </div>
                         <div>
-                            <p class="text-uppercase">View</p>
+                            <p class="text-uppercase link-light">View</p>
                         </div>
                     </div>
                 </div>
@@ -34,15 +34,15 @@
         </div>
         <div class="col-md-6 col-lg-3">
             <a href="">
-                <div class="border border-light p-2 img-hover-scale">
-                    <img src="https://picsum.photos/300/250?random=1" alt="" class="img-hover-scale mb-3 w-100">
+                <div class="border border-light p-2 hover-rotate">
+                    <img src="https://picsum.photos/300/250?random=1" alt="" class="mb-3 w-100">
 
                     <div class="d-flex justify-content-between align-items-center mx-3">
                         <div>
                             <p class="fs-4 text-uppercase fw-bold text-white">Tops</p>
                         </div>
                         <div>
-                            <p class="text-uppercase">View</p>
+                            <p class="text-uppercase link-light">View</p>
                         </div>
                     </div>
                 </div>
@@ -50,15 +50,15 @@
         </div>
         <div class="col-md-6 col-lg-3">
             <a href="">
-                <div class="border border-light p-2 img-hover-scale">
-                    <img src="https://picsum.photos/300/250?random=1" alt="" class="img-hover-scale mb-3 w-100">
+                <div class="border border-light p-2 hover-rotate">
+                    <img src="https://picsum.photos/300/250?random=1" alt="" class="mb-3 w-100">
 
                     <div class="d-flex justify-content-between align-items-center mx-3">
                         <div>
                             <p class="fs-4 text-uppercase fw-bold text-white">Bottoms</p>
                         </div>
                         <div>
-                            <p class="text-uppercase">View</p>
+                            <p class="text-uppercase link-light">View</p>
                         </div>
                     </div>
                 </div>
@@ -66,15 +66,15 @@
         </div>
         <div class="col-md-6 col-lg-3">
             <a href="">
-                <div class="border border-light p-2 img-hover-scale">
-                    <img src="https://picsum.photos/300/250?random=1" alt="" class="img-hover-scale mb-3 w-100">
+                <div class="border border-light p-2 hover-rotate">
+                    <img src="https://picsum.photos/300/250?random=1" alt="" class="mb-3 w-100">
 
                     <div class="d-flex justify-content-between align-items-center mx-3">
                         <div>
                             <p class="fs-4 text-uppercase fw-bold text-white">Accessories</p>
                         </div>
                         <div>
-                            <p class="text-uppercase">View</p>
+                            <p class="text-uppercase link-light">View</p>
                         </div>
                     </div>
                 </div>
@@ -82,15 +82,15 @@
         </div>
         <div class="col-md-6 col-lg-3">
             <a href="">
-                <div class="border border-light p-2 img-hover-scale">
-                    <img src="https://picsum.photos/300/250?random=1" alt="" class="img-hover-scale mb-3 w-100">
+                <div class="border border-light p-2 hover-rotate">
+                    <img src="https://picsum.photos/300/250?random=1" alt="" class="mb-3 w-100">
 
                     <div class="d-flex justify-content-between align-items-center mx-3">
                         <div>
                             <p class="fs-4 text-uppercase fw-bold text-white">Jewelry</p>
                         </div>
                         <div>
-                            <p class="text-uppercase">View</p>
+                            <p class="text-uppercase link-light">View</p>
                         </div>
                     </div>
                 </div>
@@ -98,15 +98,15 @@
         </div>
         <div class="col-md-6 col-lg-3">
             <a href="">
-                <div class="border border-light p-2 img-hover-scale">
-                    <img src="https://picsum.photos/300/250?random=1" alt="" class="img-hover-scale mb-3 w-100">
+                <div class="border border-light p-2 hover-rotate">
+                    <img src="https://picsum.photos/300/250?random=1" alt="" class="mb-3 w-100">
 
                     <div class="d-flex justify-content-between align-items-center mx-3">
                         <div>
                             <p class="fs-4 text-uppercase fw-bold text-white">Kids</p>
                         </div>
                         <div>
-                            <p class="text-uppercase">View</p>
+                            <p class="text-uppercase link-light">View</p>
                         </div>
                     </div>
                 </div>
@@ -114,15 +114,15 @@
         </div>
         <div class="col-md-6 col-lg-3">
             <a href="">
-                <div class="border border-light p-2 img-hover-scale">
-                    <img src="https://picsum.photos/300/250?random=1" alt="" class="img-hover-scale mb-3 w-100">
+                <div class="border border-light p-2 hover-rotate">
+                    <img src="https://picsum.photos/300/250?random=1" alt="" class="mb-3 w-100">
 
                     <div class="d-flex justify-content-between align-items-center mx-3">
                         <div>
                             <p class="fs-4 text-uppercase fw-bold text-white">Perfumes</p>
                         </div>
                         <div>
-                            <p class="text-uppercase">View</p>
+                            <p class="text-uppercase link-light">View</p>
                         </div>
                     </div>
                 </div>
